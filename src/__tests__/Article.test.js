0d1a2ee9027ba31ef69bf531c9ca0ebc67ce5133
shirocodes <ishirapt@gmail.com>
2025-04-07 17:16:19 +0300

    Complete lab

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -3,9 +3,11 @@ import { render, screen } from "@testing-library/react";
 import React from "react";
 
 import Article from "../components/Article";
-
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
+
+  // add this line
+  screen.debug();
 
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
